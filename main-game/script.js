e73dbad519bdca056e587f4e4ad2cd331ed412c3
prashanthans02 <prashanthanand873@gmail.com>
2026-05-23 05:20:38 +0530

    Adding the object for the game flow, it cotains the players in an array a function to switch players, getting the active player and play round which takes the location as parameter and sets the value of that location that current player's mark then switches the player.

--- main-game/script.js
+++ main-game/script.js
@@ -1,5 +1,5 @@
 // function to make the players 
-players = (name, mark) => {
+player = (name, mark) => {
     return {
         name: name,
         mark: mark
@@ -11,3 +11,36 @@ const gameBoard = {
     board: ["", "", "", "", "", "", "", "", ""]
 };
 
+// Object for the game flow 
+const gameControl = {
+    players: [
+        player1 = player("bob", "C"),
+        player2 = player("alex", "O")
+    ],
+
+    // function to switch player 
+    activePlayerIndex: 0,
+
+    switchPlayer() {
+        if (this.activePlayerIndex === 0) {
+            this.activePlayerIndex = 1; 
+        } else {
+            this.activePlayerIndex = 0;
+        }
+    },
+
+    // getting  current player 
+    getActivePlayer() {
+        return this.players[this.activePlayerIndex]
+    },
+    
+    // updating the cells of the board 
+    playRound(location) {
+        let activePlayer = this.getActivePlayer();
+        gameBoard.board[location] = activePlayer.mark;
+        this.switchPlayer();
+        console.log(gameBoard.board);
+    },
+};
+
+gameControl.playRound(3);
